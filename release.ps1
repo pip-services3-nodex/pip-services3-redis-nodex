a55c21c7449288be9f91323be19d73772a0e7638
Dmitry Krayniy <krdima92@gmail.com>
2022-06-04 15:14:36 +0400

    Ouput npm version from gh runner

--- release.ps1
+++ release.ps1
@@ -33,10 +33,11 @@ Write-Host "Getting $($package.name) versions from npm registry..."
 # Check if version exist on npmjs
 $npmjsPackageVersionsRaw = npm view $package.name versions
 node -v
+npm -v
 Write-Host $npmjsPackageVersionsRaw 
 if ($npmjsPackageVersionsRaw[0] -ne "[") { # if npm returned str instead of array
     Write-Host "Updating npm view responce"
-    $npmjsPackageVersionsRaw = "[ $npmjsPackageVersionsRaw ]"
+    $npmjsPackageVersionsRaw = "[ '$npmjsPackageVersionsRaw' ]"
 }
 Write-Host $npmjsPackageVersionsRaw
 $npmjsPackageVersions = $npmjsPackageVersionsRaw | ConvertFrom-Json
